d98438f55e44290365ec8e71c87b0b3eecbb7efa
shebeko79 <eugene.shebeko@gmail.com>
2025-04-01 21:22:47 +0200

    Rework all tool

--- yolo_labeling/main.py
+++ yolo_labeling/main.py
@@ -2,13 +2,10 @@ import sys
 import os
 import cv2
 import json
-import numpy as np
-from PIL import Image, ExifTags
-from glob import glob
-from PyQt5.QtCore import Qt, QCoreApplication
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QComboBox
 from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QFileDialog, QLabel
-from PyQt5.QtWidgets import QDesktopWidget, QMessageBox, QCheckBox
+from PyQt5.QtWidgets import QDesktopWidget, QMessageBox, QPushButton
 from PyQt5.QtGui import QPixmap, QPainter, QBrush, QColor, QPen, QFont
 from PyQt5.QtCore import QRect, QPoint
 
@@ -60,6 +57,7 @@ class MainWidget(QWidget):
 
         self.loadConfig()
         self.initImagesDirectory()
+        self.fillClassesCombo()
         self.setCurrentImage()
         self.check_controls()
 
@@ -67,24 +65,48 @@ class MainWidget(QWidget):
     def initUI(self):
         self.label_img = ImageWidget(self)
 
-        self.prevButton = QPushButton('Prev', self)
+        self.firstButton = QPushButton('First', self)
+        self.firstButton.clicked.connect(self.firstImage)
+
+        self.prevButton = QPushButton('<', self)
         self.prevButton.clicked.connect(self.prevImage)
 
-        self.nextButton = QPushButton('Next', self)
+        self.nextButton = QPushButton('>', self)
         self.nextButton.clicked.connect(self.nextImage)
 
-        self.clearButton = QPushButton('Clear', self)
-        self.clearButton.clicked.connect(self.label_img.cancelLast)
+        self.rewindButton = QPushButton('>>>', self)
+        self.rewindButton.clicked.connect(self.nextUnmarkedImage)
+
+        self.lastButton = QPushButton('Last', self)
+        self.lastButton.clicked.connect(self.lastImage)
+
+        self.removeLastButton = QPushButton('Remove Last', self)
+        self.removeLastButton.clicked.connect(self.label_img.removeLast)
+
+        self.removeAllButton = QPushButton('Remove All', self)
+        self.removeAllButton.clicked.connect(self.label_img.removeAll)
+
+        self.undoButton = QPushButton('Undo', self)
+        self.undoButton.clicked.connect(self.label_img.reload)
+
+        self.classesCombo = QComboBox(self)
+        self.classesCombo.setFixedWidth(200)
+        self.classesCombo.activated[int].connect(self.onClassesComboChanged)
 
         hbox = QHBoxLayout()
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.clearButton)
-        hbox.addLayout(vbox)
+        hbox.addWidget(self.classesCombo)
+        hbox.addStretch(1)
+        hbox.addWidget(self.removeLastButton)
+        hbox.addWidget(self.removeAllButton)
+        hbox.addWidget(self.undoButton)
 
         hbox.addStretch(3)
+        hbox.addWidget(self.firstButton)
         hbox.addWidget(self.prevButton)
         hbox.addWidget(self.nextButton)
+        hbox.addWidget(self.rewindButton)
+        hbox.addWidget(self.lastButton)
 
         vbox = QVBoxLayout()
         vbox.addWidget(self.label_img)
@@ -92,18 +114,61 @@ class MainWidget(QWidget):
 
         self.setLayout(vbox)
 
+    def fillClassesCombo(self):
+
+        self.classesCombo.clear()
+        for v in enumerate(self.classes):
+            if v[0]<10:
+                self.classesCombo.addItem(f'({v[0]}) {v[1]}')
+            else:
+                self.classesCombo.addItem(v[1])
+
+
+        if len(self.classes):
+            self.classesCombo.setCurrentIndex(0)
+
+
     def check_controls(self):
         valid_img = 0 < self.cur_image < len(self.images)
+        not_first_img = self.cur_image > 0
+        not_last_img = self.cur_image+1 < len(self.images)
 
-        self.prevButton.setEnabled(self.cur_image > 0)
-        self.nextButton.setEnabled(self.cur_image+1 < len(self.images))
-        self.clearButton.setEnabled(valid_img)
+        self.firstButton.setEnabled(not_first_img)
+        self.prevButton.setEnabled(not_first_img)
+        self.nextButton.setEnabled(not_last_img)
+        self.rewindButton.setEnabled(not_last_img)
+        self.lastButton.setEnabled(not_last_img)
+
+        self.removeLastButton.setEnabled(valid_img)
+        self.removeAllButton.setEnabled(valid_img)
+        self.undoButton.setEnabled(valid_img)
         pass
+
+    def firstImage(self):
+        self.label_img.saveModifiedBoxes()
+        self.cur_image = 0
+        self.setCurrentImage()
+        self.check_controls()
+
+    def lastImage(self):
+        self.label_img.saveModifiedBoxes()
+        self.cur_image = 0
+        if len(self.images) > 0:
+            self.cur_image = len(self.images) - 1
+        self.setCurrentImage()
+        self.check_controls()
+
+    def nextUnmarkedImage(self):
+        self.label_img.saveModifiedBoxes()
+        self.rewind_to_unmarked()
+        self.setCurrentImage()
+        self.check_controls()
 
     def prevImage(self):
         if self.cur_image <= 0:
             return
 
+        self.label_img.saveModifiedBoxes()
         self.cur_image -= 1
         self.setCurrentImage()
         self.check_controls()
@@ -112,6 +177,7 @@ class MainWidget(QWidget):
         if self.cur_image >= len(self.images):
             return
 
+        self.label_img.saveModifiedBoxes()
         self.cur_image += 1
         self.setCurrentImage()
         self.check_controls()
@@ -120,7 +186,7 @@ class MainWidget(QWidget):
         if self.cur_image < 0 or self.cur_image >= len(self.images):
             self.app.fileName.setText("")
             self.app.progress.setText("")
-            self.label_img.resetResult()
+            self.label_img.removeAll()
             return
 
         img_file = self.images[self.cur_image]
@@ -164,10 +230,20 @@ class MainWidget(QWidget):
 
         self.images.sort()
 
+        self.cur_image = 0
+        self.rewind_to_unmarked()
+
+    def rewind_to_unmarked(self):
+        labels_dir = os.path.join(self.directory, "labels")
+        if self.cur_image < len(self.images) - 1:
+            filename, ext = os.path.splitext(self.images[self.cur_image])
+            file_path = os.path.join(labels_dir, filename + ".txt")
+            if not os.path.exists(file_path):
+                return
+
         txt_files = {}
-        file_name = os.path.join(self.directory, "labels")
-        if os.path.exists(file_name):
-            for entry in os.scandir(file_name):
+        if os.path.exists(labels_dir):
+            for entry in os.scandir(labels_dir):
                 if not entry.is_file():
                     continue
 
@@ -176,7 +252,6 @@ class MainWidget(QWidget):
                 if ext.lower() == '.txt':
                     txt_files[filename.lower()] = 1
 
-        self.cur_image = 0
         while self.cur_image < len(self.images) - 1:
             filename, ext = os.path.splitext(self.images[self.cur_image])
             if not (filename.lower() in txt_files):
@@ -205,29 +280,45 @@ class MainWidget(QWidget):
             json.dump(config_dict, config_file)
 
     def keyPressEvent(self, e):
-        config_len = len(self.key_config)
-        for i, key_n in enumerate(range(49,58), 1):
-            if e.key() == key_n and config_len >= i:
-                self.label_img.markBox(i-1) 
-                break
+        k = e.key()
+        if 0x30 <= e.key() <= 0x39:
+            idx = e.key()-0x30
+            if idx < self.classesCombo.count():
+                self.classesCombo.setCurrentIndex(idx)
+            self.label_img.markBox(idx)
         if e.key() == Qt.Key_Escape:
-            self.label_img.cancelLast()
-        elif e.key() == Qt.Key_E:
-            self.setNextImage()
-        elif e.key() == Qt.Key_Q:
-            self.label_img.resetResult()
+            self.label_img.removeLast()
+        elif e.key() == Qt.Key_Right:
+            self.nextImage()
+        elif e.key() == Qt.Key_Left:
+            self.prevImage()
+        elif e.key() == Qt.Key_Up:
+            sel_idx = self.classesCombo.currentIndex()
+            sel_idx -= 1
+            if sel_idx >= 0:
+                self.classesCombo.setCurrentIndex(sel_idx)
+                self.label_img.markBox(sel_idx)
+        elif e.key() == Qt.Key_Down:
+            sel_idx = self.classesCombo.currentIndex()
+            sel_idx += 1
+            if sel_idx < len(self.classes):
+                self.classesCombo.setCurrentIndex(sel_idx)
+                self.label_img.markBox(sel_idx)
 
+    def onClassesComboChanged(self, idx):
+        self.label_img.markBox(idx)
+        self.setFocus()
 
 class ImageWidget(QWidget):
-    def __init__(self, main_widget):
+    def __init__(self, main_widget: MainWidget):
         super(ImageWidget, self).__init__(main_widget.app)
         self.main_widget = main_widget
         self.app = main_widget.app
         self.results = []
         self.setMouseTracking(True)
         self.screen_height = QDesktopWidget().screenGeometry().height()
-        self.last_idx = 0
         self.txt_file = ""
+        self.modified = False
 
         self.initUI()
 
@@ -247,10 +338,7 @@ class ImageWidget(QWidget):
         self.txt_file = txt_file
         self.loadBoxes()
         self.pixmap = self.drawResultBox()
-
         self.update()
-
-
 
     def setPixmap(self, image_fn):
         self.pixmap = QPixmap(image_fn)
@@ -287,8 +375,7 @@ class ImageWidget(QWidget):
                     break
 
     def mouseMoveEvent(self, event):
-        self.app.cursorPos.setText('({}, {})'
-                                      .format(event.pos().x(), event.pos().y()))
+        self.app.cursorPos.setText(f'({event.pos().x()}, {event.pos().y()})')
         if event.buttons() and Qt.LeftButton and self.drawing:
             self.pixmap = QPixmap.copy(self.prev_pixmap)
             painter = QPainter(self.pixmap)
@@ -300,29 +387,23 @@ class ImageWidget(QWidget):
             self.update()
 
     def mouseReleaseEvent(self, event):
-        if event.button() == Qt.LeftButton:
-            p1_x, p1_y = self.lastPoint.x(), self.lastPoint.y()
-            p2_x, p2_y = event.pos().x(), event.pos().y()
-            lx, ly = min(p1_x, p2_x), min(p1_y, p2_y)
-            w, h = abs(p1_x - p2_x), abs(p1_y - p2_y)
-            if (p1_x, p1_y) != (p2_x, p2_y):
-                if self.results and (len(self.results[-1]) == 4):
-                    self.showPopupOk('warning message',
-                                     'Please mark the box you drew.')
-                    self.pixmap = self.drawResultBox()
-                    self.update()
-                else:
-                    self.results.append([lx, ly, lx + w, ly + h])
-                self.drawing = False
+        if event.button() != Qt.LeftButton:
+            return
 
-    def showPopupOk(self, title: str, content: str):
-        msg = QMessageBox()
-        msg.setWindowTitle(title)
-        msg.setText(content)
-        msg.setStandardButtons(QMessageBox.Ok)
-        result = msg.exec_()
-        if result == QMessageBox.Ok:
-            msg.close()
+        p1_x, p1_y = self.lastPoint.x(), self.lastPoint.y()
+        p2_x, p2_y = event.pos().x(), event.pos().y()
+        lx, ly = min(p1_x, p2_x), min(p1_y, p2_y)
+        w, h = abs(p1_x - p2_x), abs(p1_y - p2_y)
+        if (p1_x, p1_y) == (p2_x, p2_y):
+            return
+
+        self.drawing = False
+        self.results.append([lx, ly, lx + w, ly + h, 0])
+
+        sel_idx = self.main_widget.classesCombo.currentIndex()
+        if sel_idx < 0:
+            sel_idx = 0
+        self.markBox(sel_idx)
 
     def drawResultBox(self):
         res = QPixmap.copy(self.pixmapOriginal)
@@ -333,37 +414,46 @@ class ImageWidget(QWidget):
         for box in self.results:
             lx, ly, rx, ry = box[:4]
             painter.drawRect(lx, ly, rx - lx, ry - ly)
-            if len(box) == 5:
+            idx = box[4]
+            if 0 <= idx < len(self.main_widget.classes):
                 painter.setPen(QPen(Qt.blue, 2, Qt.SolidLine))
-                painter.drawText(lx, ly + 15, self.main_widget.classes[box[-1]])
+                painter.drawText(lx, ly + 15, self.main_widget.classes[idx])
                 painter.setPen(QPen(Qt.red, 2, Qt.SolidLine))
         return res
 
-    def cancelLast(self):
+    def removeLast(self):
         if len(self.results)>0:
             self.results.pop()  # pop last
+            self.modified = True
             self.pixmap = self.drawResultBox()
             self.update()
+            self.main_widget.check_controls()
 
-    def resetResult(self):
+    def removeAll(self):
         self.results = []
+        self.modified = True
         self.pixmap = self.drawResultBox()
         self.update()
+        self.main_widget.check_controls()
+
+    def reload(self):
+        self.loadBoxes()
+        self.pixmap = self.drawResultBox()
+        self.update()
+        self.main_widget.check_controls()
 
     def markBox(self, idx):
-        self.last_idx = idx
-        if self.results:
-            if len(self.results[-1]) == 4:
-                self.results[-1].append(idx)
-            elif len(self.results[-1]) == 5:
-                self.results[-1][-1] = idx
-            else:
-                raise ValueError('invalid results')
-            self.pixmap = self.drawResultBox()
-            self.update()
+        if len(self.results)>0:
+            self.results[-1][-1] = idx
+
+        self.modified = True
+        self.pixmap = self.drawResultBox()
+        self.update()
+        self.main_widget.check_controls()
 
     def loadBoxes(self):
         self.results = []
+        self.modified = False
 
         if self.txt_file == '' or not os.path.exists(self.txt_file):
             return
@@ -384,11 +474,11 @@ class ImageWidget(QWidget):
                     vls = [int((cx-w/2)*self.W), int((cy-h/2)*self.H), int((cx+w/2)*self.W), int((cy+h/2)*self.H), idx]
                     self.results.append(vls)
 
-    def saveBoxes(self):
-        if self.txt_file == '':
+    def saveModifiedBoxes(self):
+        if self.txt_file == '' or not self.modified:
             return
 
-        with open(self.file_name, 'w') as file:
+        with open(self.txt_file, 'w') as file:
             for elements in self.results:
                 lx, ly, rx, ry, idx = elements
                 cx = (lx + rx) / 2 / self.W
